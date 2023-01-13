61f65cc4a2d68ebac6f8433ed030f81e058190ad
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-01-13 10:39:29 +0300

    controllers functions

---
+++ server/Src/controllers/post.js
@@ -0,0 +1,8 @@
+//Create Post
+export const createPost = async (req, res) => {};
+//Get All the posts
+export const getAllPosts = async (req, res) => {};
+//Update post
+export const updatePost = async (req, res) => {};
+//delete post
+export const deletePost = async (req, res) => {};
