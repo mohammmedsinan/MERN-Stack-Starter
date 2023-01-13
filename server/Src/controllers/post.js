e5039eb433acfa215bdabedb4d988729de62ce69
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-01-13 11:44:31 +0300

    create post api

--- server/Src/controllers/post.js
+++ server/Src/controllers/post.js
@@ -1,5 +1,23 @@
+import Post from '../models/post.js';
+
 //Create Post
-export const createPost = async (req, res) => {};
+export const createPost = async (req, res) => {
+  const postContext = req.body;
+  const createNewPost = new Post(postContext);
+  try {
+    await createNewPost.save().then((postData) => {
+      res.status(201).json({
+        message: 'Post Created Successfully',
+        data: postData,
+      });
+    });
+  } catch (err) {
+    res.status(401).json({
+      message: 'create post failed',
+      error: err,
+    });
+  }
+};
 //Get All the posts
 export const getAllPosts = async (req, res) => {};
 //Update post
