580122601720659258a176a8338a7842d3d34277
mohammmedsinan <75750652+mohammmedsinan@users.noreply.github.com>
2023-01-13 10:52:00 +0300

    add all routes

--- server/Src/routes/post.js
+++ server/Src/routes/post.js
@@ -1,6 +1,11 @@
 import express from 'express';
-import {} from '../controllers/post.js';
+import { createPost, deletePost, getAllPosts, updatePost } from '../controllers/post.js';
 
 const router = express.Router();
+
+router.get('/get', getAllPosts);
+router.get('/create', createPost);
+router.put('/update/:id', updatePost);
+router.delete('/delete/:id', deletePost);
 
 export default router;
